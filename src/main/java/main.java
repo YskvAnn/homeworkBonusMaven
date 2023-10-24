43f0fecdc8f4c08bf28b321e4b5f53bfcd9d892b
AnnNetoTest <yaskovaann2002@mail.ru>
2023-10-24 19:34:57 +0300

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+
+        System.out.println(actual);
+    }
+}
